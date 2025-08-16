bfa4f4fbb9cd271773a2df9bf44839fe064fba09
dameradev <radevdamjan@gmail.com>
2025-08-16 21:46:14 +0200

    Header adjustment

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,20 +3,20 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className="bg-gradient-to-r  text-white shadow-lg">
-      <div className="container mx-auto px-4 py-4">
+      <div className=" mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
-            <h1 className="text-2xl md:text-3xl font-bold tracking-wide flex items-center gap-2">
             <Image
               src="/football-icon.png"
               alt="Football Icon"
               width={40}
-              height={40}
-              className="w-10 h-10 md:w-12 md:h-12"
+              height={20}
+              className="w-10 h-8 md:w-12 md:h-10 mr-2"
               priority
             />
+            <span className='ml-2'>
               Fantasy Football
-            </h1>
+            </span>
           </div>
         </div>
       </div>
